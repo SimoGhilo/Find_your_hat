270ca92a93c4c22a7d42efdf83a3a6962656154c
SimoGhilo <simoneghilotti@gmail.com>
2022-10-19 18:10:22 +0100

    19.10.22

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const path = '*';
+let run = true;
 
 let randomArray = ['*', '^', '░', 'O'];
 let randomIndex = Math.floor(Math.random() * 4);
@@ -21,20 +22,20 @@ class Field {
         for (let y = 0; y < height; y++) {
             for (let x = 0; x < width; x++) {
                 const prob = Math.random();
-                field[y][x] = prob > percentage ? fieldCharacter : hole;
+                field[x][y] = prob > percentage ? fieldCharacter : hole;
             }
         }
-        // Set the "hat" location
+        // Set the hat location
         const hatLocation = {
             x: Math.floor(Math.random() * width),
             y: Math.floor(Math.random() * height)
         };
-        // Make sure the "hat" is not at the starting point
+
         while (hatLocation.x === 0 && hatLocation.y === 0) {
             hatLocation.x = Math.floor(Math.random() * width);
             hatLocation.y = Math.floor(Math.random() * height);
         }
-        field[hatLocation.y][hatLocation.x] = hat;
+        field[hatLocation.x][hatLocation.y] = hat;
         return field;
     }
 
@@ -49,45 +50,41 @@ class Field {
     }
 
     outOfBound() {
-        this.xLocation >= 0 &&
-            this.yLocation >= 0 &&
-            this.xLocation < this.field.width &&
-            this.yLocation < this.field.length
+        console.log(this.xLocation, this.yLocation)
+        console.log(`array lenght${this.field.length}, nested array ${this.field[0].lenght}`)
+        if (this.xLocation < 0 || this.yLocation < 0 || this.xLocation >= this.field.length || this.yLocation >= this.field.length) {
+            return true;
+
+        }
+        // this.yLocation >= 0 &&
+        //  this.xLocation < this.field.width &&
+        // this.yLocation < this.field.length
     }
 
     prompt() {
         const input = prompt('Where would you like to move?');
         switch (input) {
             case 'up':
-                this.ylocation += 1;
+                this.xLocation -= 1;
                 break;
             case 'down':
-                this.ylocation -= 1;
+                this.xLocation += 1;
                 break;
             case 'left':
-                this.xlocation -= 1;
+                this.yLocation -= 1;
                 break;
             case 'right':
-                this.xlocation += 1;
+                this.yLocation += 1;
                 break;
             default:
                 console.log('Enter one of the following: up,down, left or right: ');
                 this.prompt();
                 break;
         }
-        this.field[this.xLocation][this.yLocation] = path;
-    }
-
-    print() {
-        const display = this.field.map(row => row.join('')).join('\n');
-        return display;
-    }
-
-    Game() {
-        let run = true;
-        while (run) {
-            console.log(this.print());
-            this.prompt();
+        if (this.outOfBound()) {
+            console.log('You went out of bound !')
+            run = false;
+        } else {
             if (this.hitHat()) {
                 console.log('You won!');
                 run = false;
@@ -96,9 +93,23 @@ class Field {
                 console.log('You fell into a hole!');
                 run = false;
             }
-            if (this.outOfBound()) {
-                console.log('You went out of bound !')
-            }
+        }
+
+        if (run) {
+            this.field[this.xLocation][this.yLocation] = path;
+        }
+
+    }
+
+    print() {
+        const display = this.field.map(row => row.join('')).join('\n');
+        return display;
+    }
+
+    Game() {
+        while (run) {
+            console.log(this.print());
+            this.prompt();
         }
     }
 }
